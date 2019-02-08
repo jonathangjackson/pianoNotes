870f5182a8b7ed913d5fe5ad1067bd6d8227ca99
Jonathan Jackson <jack0355@algonquinlive.com>
2019-02-08 10:13:31 -0500

    assign 2

--- app.js
+++ app.js
@@ -6,38 +6,28 @@ const socketIO = require('socket.io')(server);
 
 const LISTEN_PORT = 8080;
 
-app.get('/color', function(req, res) {
-    res.sendFile(__dirname + '/public/color.html');
+app.get('/mobile', function(req, res) {
+    res.sendFile(__dirname + '/public/mobile.html');
 });
 
-app.get('/controller', function(req, res) {
-    res.sendFile(__dirname + '/public/controller.html');
+app.get('/desktop', function(req, res) {
+    app.use(express.static('Public/assets'));
+    res.sendFile(__dirname + '/public/desktop.html');
 });
 
 socketIO.on('connection', function(socket){
     console.log(socket.id + " has connected!");
 
     socket.on('disconnect', function(data){
-        console.log(socekt.id + " has disconnected");
+        console.log(socket.id + " has disconnected");
     });
     //socket = one client
     //socketIO.socekts = all cliets
+    /*
     socket.on('red', function(data){
         console.log('red event heard');
         socketIO.sockets.emit('color_change', {r: 255, g: 0, b: 0});
-    });
-
-    socket.on('green', function(data){
-        console.log('green event heard');
-        socketIO.sockets.emit('color_change', {r: 0, g: 255, b: 0});
-    });
-
-    socket.on('blue', function(data){
-        console.log('blue event heard');
-        socketIO.sockets.emit('color_change', {r: 0, g: 0, b: 255});
-    });
-
-
+    });*/
 });
 
 server.listen(LISTEN_PORT);
